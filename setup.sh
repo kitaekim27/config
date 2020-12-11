e98b2204a10c8245dad7f85ac407ae91fd1b22c4
Kitae Kim <develoot@gmail.com>
2020-12-11 22:22:36 +0900

    Rewrite main script in POSIX-compatible style

--- setup.sh
+++ setup.sh
@@ -1,69 +1,77 @@
-#!/usr/bin/env bash
+#!/bin/sh -eu
 
-set -euo pipefail
-
-function setup_packages
+setup_basic_tools()
 {
 	sudo apt-get install -yq build-essential curl wget gdb git net-tools bear
 }
 
-function setup_fcitx
+setup_fcitx_hangul()
 {
 	sudo apt-get install -yq fcitx-hangul
-	im-config -n fcitx
+	im-config -n fcitx # Set default input method.
 }
 
-function setup_zsh
+setup_zsh()
 {
 	sudo apt-get install -yq zsh
-	sudo chsh -s /usr/bin/zsh ${USER}
+	sudo chsh -s /usr/bin/zsh ${USER} # Change default login shell.
+
+	# Install oh-my-zsh, a zsh configuration framework.
+	# Check [https://github.com/ohmyzsh/ohmyzsh] for detailed description.
+	[ -d ${HOME}/.oh-my-zsh ] && rm -rf ${HOME}/.oh-my-zsh
 	curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | zsh
-	cp ${PWD}/dotfiles/zshrc ${HOME}/.zshrc
+	install ${PWD}/dotfiles/zshrc ${HOME}/.zshrc
 }
 
-function setup_nvm
+setup_nvm()
 {
+	# Node Version Manager.
+	# Check [https://github.com/nvm-sh/nvm] for detailed description.
 	wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | zsh
-	. ~/.zshrc
+	source ~/.zshrc # TOOD: This command freqeuntly fails due to unexpected newline error.
 	[ command -v nvm ] && nvm install node
 }
 
-function setup_neovim
+setup_neovim()
 {
+	# Fork of good old vim.
+	# Check [https://github.com/neovim/neovim] for detailed description.
 	sudo apt-get install -yq neovim
-	# Install vim-plug. (see: https://github.com/junegunn/vim-plug)
-	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim \
-		--create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-	[ -d ${HOME}/.config/nvim ] || mkdir -p ${HOME}/.config/nvim
-	cp "${PWD}/dotfiles/init.vim" ${HOME}/.config/nvim/init.vim
+
+	# Install vim-plug. Check [https://github.com/junegunn/vim-plug].
+	curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim \
+		--create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	install -D "${PWD}/dotfiles/init.vim" ${HOME}/.config/nvim/init.vim
 	nvim --headless +PlugInstall +qa
 }
 
-function setup_emacs
+setup_emacs()
 {
 	sudo apt-get install -yq emacs
+	systemctl --user enable emacs # Launch emacs server automatically.
+
+	# Install spacemacs, a emacs configuration framework.
+	# Check [https://www.spacemacs.org/] for detailed description.
 	[ -d ${HOME}/.emacs.d ] && rm -rf ${HOME}/.emacs.d
 	git clone -q https://github.com/syl20bnr/spacemacs ${HOME}/.emacs.d
-	systemctl --user enable emacs # Launch emacs server automatically.
 }
 
-function setup_tmux
+setup_tmux()
 {
 	sudo apt-get install -yq tmux
-	cp "${PWD}/dotfiles/tmux.conf" ${HOME}/.tmux.conf
+	install "${PWD}/dotfiles/tmux.conf" ${HOME}/.tmux.conf
 }
 
-function setup_gef
+setup_gef()
 {
-	sh -c "$(wget -q -O- https://github.com/hugsy/gef/raw/master/scripts/gef.sh)"
+	# Extended GDB.
+	# Check [https://github.com/hugsy/gef] for detailed description.
+	wget -q -O- https://github.com/hugsy/gef/raw/master/scripts/gef.sh
 	command -v pip3 &> /dev/null || sudo apt-get install -yq python3-pip
-	pip3 -q install unicorn
-	pip3 -q install capstone
-	pip3 -q install ropper
-	pip3 -q install keystone
+	pip3 -q install unicorn capstone ropper keystone
 }
 
-function setup_lxd
+setup_lxd()
 {
 	sudo apt-get install -yq lxd
 	sudo usermod -aG lxd ${USER}
@@ -72,25 +80,17 @@ function setup_lxd
 	install ${PWD}/scripts/lxd/* ${HOME}/.local/bin/
 }
 
-function main
-{
-	sudo apt-get update -yq
-	sudo apt-get upgrade -yq
+sudo apt-get update -yq
+sudo apt-get upgrade -yq
 
-	setup_packages
-	setup_fcitx
-	setup_zsh
-	setup_nvm
-	setup_neovim
-	# setup_emacs
-	setup_tmux
-	setup_gef
-	setup_lxd
+setup_basic_tools
+setup_fcitx_hangul
+setup_zsh
+setup_nvm
+setup_neovim
+setup_tmux
+setup_gef
+setup_lxd
 
-	sudo apt-get autoclean -yq
-	sudo apt-get autoremove -yq
-
-	echo "All processes complete!"
-}
-
-main
+sudo apt-get autoclean -yq
+sudo apt-get autoremove -yq
