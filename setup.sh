2ee531014dd173625604879d6a0413632d73b4e7
Kitae Kim <develoot@gmail.com>
2021-08-05 20:40:54 +0900

    Clean up the installation script

--- setup.sh
+++ setup.sh
@@ -2,33 +2,41 @@
 
 set -eu
 
+POST_INSTALL_INSTRUCTION=""
+
 setup_basic_tools()
 {
-	sudo apt-get install -yq build-essential curl wget gdb git net-tools bear clang clangd \
+	sudo apt-get install -yq \
 		python-is-python3 python3-pip
+		bear clangd \
+		build-essential \
+		curl wget \
+		gdb git \
+		net-tools \
 }
 
 setup_fcitx_hangul()
 {
 	sudo apt-get install -yq fcitx-hangul
-	im-config -n fcitx # Set fcitx a default input method.
+	im-config -n fcitx # Set the fcitx as a default input method.
+	POST_INSTALL_INSTRUCTION+="[fcitx] Set Korean as a first language.\n"
 }
 
 setup_zsh()
 {
 	sudo apt-get install -yq zsh
-	sudo chsh -s /usr/bin/zsh ${USER} # Change default login shell.
+	sudo chsh -s /usr/bin/zsh "${USER}" # Set the zsh as a default login shell.
 
 	# Install oh-my-zsh, a zsh configuration framework.
 	# Check [https://github.com/ohmyzsh/ohmyzsh] for detailed description.
-	[ -d ${HOME}/.oh-my-zsh ] && rm -rf ${HOME}/.oh-my-zsh
+	[ -d "${HOME}/.oh-my-zsh" ] && rm -rf "${HOME}/.oh-my-zsh"
 	curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | zsh
-	install ${PWD}/dotfiles/zshrc ${HOME}/.zshrc
+	install "${PWD}/dotfiles/zshrc" "${HOME}/.zshrc"
 }
 
 setup_nvm()
 {
-	# Node Version Manager.
+	# Install nvm, a node version manager.
 	# Check [https://github.com/nvm-sh/nvm] for detailed description.
 	wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | zsh
 	[ command -v nvm ] && zsh -c "nvm install node"
@@ -36,38 +44,39 @@ setup_nvm()
 
 setup_neovim()
 {
-	# Fork of the good old vim.
+	# Install neovim, a modern fork of the good old vim.
 	# Check [https://github.com/neovim/neovim] for detailed description.
 	sudo apt-get install -yq neovim
 
-	# Install vim-plug. Check [https://github.com/junegunn/vim-plug].
-	curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim \
+	# Install vim-plug, a vim plugin manager.
+	# Check [https://github.com/junegunn/vim-plug] for detailed description.
+	curl -fLo "$HOME/.local/share/nvim/site/autoload/plug.vim"
 		--create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	install -D "${PWD}/dotfiles/init.vim" ${HOME}/.config/nvim/init.vim
+	install -D "${PWD}/dotfiles/init.vim" "${HOME}/.config/nvim/init.vim"
 	nvim --headless +PlugInstall +qa
-	cat ${PWD}/dotfiles/coc-config.vim >> ${HOME}/.config/nvim/init.vim
+	cat "${PWD}/dotfiles/coc-config.vim" >> "${HOME}/.config/nvim/init.vim"
 }
 
 setup_emacs()
 {
 	sudo apt-get install -yq emacs
-	systemctl --user enable emacs # Launch emacs server automatically.
+	systemctl --user enable emacs # Launch emacs daemon automatically.
 
-	# Install spacemacs, a emacs configuration framework.
+	# Install spacemacs, a modern emacs configuration framework.
 	# Check [https://www.spacemacs.org/] for detailed description.
-	[ -d ${HOME}/.emacs.d ] && rm -rf ${HOME}/.emacs.d
-	git clone -q https://github.com/syl20bnr/spacemacs ${HOME}/.emacs.d
+	[ -d "${HOME}/.emacs.d" ] && rm -rf "${HOME}/.emacs.d"
+	git clone -q "https://github.com/syl20bnr/spacemacs ${HOME}/.emacs.d"
 }
 
 setup_tmux()
 {
 	sudo apt-get install -yq tmux
-	install "${PWD}/dotfiles/tmux.conf" ${HOME}/.tmux.conf
+	install "${PWD}/dotfiles/tmux.conf" "${HOME}/.tmux.conf"
 }
 
 setup_gef()
 {
-	# Extended GDB.
+	# Install GEF, an extended GDB.
 	# Check [https://github.com/hugsy/gef] for detailed description.
 	wget -qO- https://github.com/hugsy/gef/raw/master/scripts/gef.sh | zsh
 	pip -q install capstone unicorn keystone-engine ropper
@@ -76,9 +85,9 @@ setup_gef()
 setup_lxd()
 {
 	sudo apt-get install -yq lxd
-	sudo usermod -aG lxd ${USER}
-	yes '' | lxd init # Initialize the LXD with default configurations.
-	install ${PWD}/scripts/lxd/* ${HOME}/.local/bin/
+	sudo usermod -aG lxd "${USER}"
+	yes '' | lxd init # Initialize the LXD with default options.
+	install "${PWD}/scripts/lxd/*" "${HOME}/.local/bin/"
 }
 
 sudo apt-get update -yq
@@ -95,3 +104,5 @@ setup_lxd
 
 sudo apt-get autoclean -yq
 sudo apt-get autoremove -yq
+
+printf "${POST_INSTALL_INSTRUCTION}"
