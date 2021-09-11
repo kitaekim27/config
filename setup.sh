7522933e531eb45e6278419f73cfcb8fb6599c15
Kitae Kim <develoot@gmail.com>
2021-09-11 13:34:15 +0900

    zsh: Use more plugins

--- setup.sh
+++ setup.sh
@@ -29,6 +29,15 @@ setup_zsh()
     # Check [https://github.com/ohmyzsh/ohmyzsh] for detailed description.
     [ -d "${HOME}/.oh-my-zsh" ] && rm -rf "${HOME}/.oh-my-zsh"
     curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | zsh
+
+    # Install zsh-syntax-highlighting plugin, a plugin for fish-like syntax highlighting.
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git --depth=1 \
+        ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+    # Install zsh-autosuggestions plugin, a plugin for fish-like suggestions.
+    git clone https://github.com/zsh-users/zsh-autosuggestions --depth=1 \
+        ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
     install "${PWD}/dotfiles/zshrc" "${HOME}/.zshrc"
 }
 
