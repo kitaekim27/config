fe82cafa7d9b4e93c23961dacc536f0dbd6cccb0
Kitae Kim <develoot@gmail.com>
2021-12-17 23:49:31 +0900

    .

--- distro/ubuntu.sh
+++ distro/ubuntu.sh
@@ -44,7 +44,7 @@ install_bash() {
   # Install bash-it, a community driven bash configuration framework.
   [[ -d ~/.bash_it ]] && rm -rf ~/.bash_it
   git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-  ~/.bash_it/install.sh -s
+  ~/.bash_it/install.sh -saf
   sed -i "s/BASH_IT_THEME='*'/BASH_IT_THEME='easy'/p" ~/.bashrc
 
   # Copy general configuration.
