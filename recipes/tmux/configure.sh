65e1975ede67bd4adead773d94e0fb2b52839f41
Kitae Kim <develoot@gmail.com>
2022-02-08 11:02:01 +0900

    .

--- recipes/tmux/configure.sh
+++ recipes/tmux/configure.sh
@@ -2,3 +2,10 @@
 set -o errexit -o nounset -o errtrace -o pipefail
 
 install -v tmux.conf ~/.tmux.conf
+
+echo '
+# If the shell is not running on a ssh connection, create a new tmux session.
+[ -z "${SSH_CLIENT}" ] && [ -z "${SSH_TTY}" ] \
+  && command -v tmux &> /dev/null && [ -z "${TMUX}" ] \
+  && exec tmux
+' >> ~/.bashrc
