ae410707ec83a5ae00355b58dfadac102e987e26
Kitae Kim <develoot@gmail.com>
2022-02-16 08:44:07 +0900

    fzf configure

---
+++ recipes/fzf/configure.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -o errexit -o errtrace -o nounset -o pipefail
+
+echo '
+source /etc/os-release
+if [[ "${NAME}" = "Ubuntu" ]]; then
+  source /usr/share/doc/fzf/examples/completion.bash
+  source /usr/share/doc/fzf/examples/key-bindings.bash
+else
+  source /usr/share/fzf/completion.bash
+  source /usr/share/fzf/key-bindings.bash
+fi
+' >> ~/.bashrc
