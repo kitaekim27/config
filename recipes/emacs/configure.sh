7b33350e8f45248b2cfcd528cf9483b115b37e75
Kitae Kim <develoot@gmail.com>
2022-02-16 08:47:29 +0900

    emacs

--- recipes/emacs/configure.sh
+++ recipes/emacs/configure.sh
@@ -5,4 +5,10 @@ systemctl --user enable emacs # Launch emacs daemon automatically.
 # Install spacemacs, a modern emacs configuration framework.
 # Check [https://www.spacemacs.org/] for detailed description.
 [ -d "${HOME}/.emacs.d" ] && rm -rf "${HOME}/.emacs.d"
-git clone -q "https://github.com/syl20bnr/spacemacs ${HOME}/.emacs.d"
+git clone -q --depth=1 "https://github.com/syl20bnr/spacemacs ${HOME}/.emacs.d"
+
+echo '
+export EDITOR="emacsclient -tc"
+alias emacs="emacsclient -tc"
+alias e="emacsclient -tc"
+' >> ~/.bashrc
