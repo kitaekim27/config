dec7513a651dfdb365d576c07dc286e3bfce0cf1
Kitae Kim <develoot@gmail.com>
2022-05-04 21:25:26 +0900

    .

--- recipes/zsh/configure.sh
+++ recipes/zsh/configure.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -o errexit -o nounset -o errtrace -o pipefail
 
-sudo chsh -s /usr/bin/zsh "${USER}" # Set the zsh as a default login shell.
+install -v $(dirname $0)/zshrc ~/.zshrc
 
 # Install oh-my-zsh, a zsh configuration framework.
 # Check [https://github.com/ohmyzsh/ohmyzsh] for detailed description.
 [ -d "${HOME}/.oh-my-zsh" ] && rm -rf "${HOME}/.oh-my-zsh"
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | zsh
 
-install -v $(dirname $0)/zshrc ~/.zshrc
+sudo chsh -s /usr/bin/zsh "${USER}" # Set the zsh as a default login shell.
