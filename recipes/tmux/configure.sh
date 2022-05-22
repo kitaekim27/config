6b22eaa738c71b30884429aee0c707a847744119
Kitae Kim <develoot@gmail.com>
2022-05-22 22:11:54 +0900

    .

--- recipes/tmux/configure.sh
+++ recipes/tmux/configure.sh
@@ -4,7 +4,6 @@ set -o errexit -o nounset -o errtrace -o pipefail
 install -v $(dirname $0)/tmux.conf ~/.tmux.conf
 
 cat << 'EOF' >> ~/.zshrc
-
 command -v tmux &> /dev/null \
   && [ -z "${SSH_CLIENT}" ] \
   && [ -z "${SSH_TTY}" ] \
