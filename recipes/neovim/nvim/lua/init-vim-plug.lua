7550346778c79049d8252257b76b1572638c3fc4
Kitae Kim <develoot@gmail.com>
2022-06-03 06:29:47 +0900

    Do not use plugin auto-pairs

--- recipes/neovim/nvim/lua/init-vim-plug.lua
+++ recipes/neovim/nvim/lua/init-vim-plug.lua
@@ -2,7 +2,6 @@ vim.cmd([[
     call plug#begin(stdpath('data') . '/plugged')
     Plug 'bronson/vim-trailing-whitespace'
     Plug 'christoomey/vim-tmux-navigator'
-    Plug 'jiangmiao/auto-pairs'
     Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
     Plug 'junegunn/fzf.vim'
     Plug 'neovim/nvim-lspconfig'
