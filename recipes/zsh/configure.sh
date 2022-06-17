8afd6d39c1191400f17804a05f535a68d2408d45
Kitae Kim <develoot@gmail.com>
2022-06-17 19:15:25 +0900

    Use zsh plugins

--- recipes/zsh/configure.sh
+++ recipes/zsh/configure.sh
@@ -4,9 +4,13 @@ set -o errexit -o nounset -o errtrace -o pipefail
 # Install oh-my-zsh, a zsh configuration framework.
 # Check [https://github.com/ohmyzsh/ohmyzsh] for detailed description.
 [ -d "${HOME}/.oh-my-zsh" ] && rm -rf "${HOME}/.oh-my-zsh"
-curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
+# Install basic zshrc.
 install -v $(dirname $0)/zshrc ~/.zshrc
+# Install fish-like autosuggestion plugin for zsh.
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+# Install fish-like syntax highlighting plugin for zsh.
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-sudo chsh -s /usr/bin/zsh "${USER}" # Set the zsh as a default login shell.
-
+sudo chsh -s $(which zsh) "${USER}" # Set the zsh as a default login shell.
