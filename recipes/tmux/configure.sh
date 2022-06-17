62a022b718eea87425bd4047d53dd4de0125dd19
Kitae Kim <develoot@gmail.com>
2022-06-17 17:23:52 +0900

    Use Tmux Plugin Manager and extrakto plugin

--- recipes/tmux/configure.sh
+++ recipes/tmux/configure.sh
@@ -3,6 +3,8 @@ set -o errexit -o nounset -o errtrace -o pipefail
 
 install -v $(dirname $0)/tmux.conf ~/.tmux.conf
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 cat << 'EOF' >> ~/.zshrc
 command -v tmux &> /dev/null \
   && [ -z "${SSH_CLIENT}" ] \
